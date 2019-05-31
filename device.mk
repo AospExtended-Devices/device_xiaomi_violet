42708a3bb92a75a3e954eaec4b371dac13637ffb
Atman <masteratman@gmail.com>
2019-05-31 18:03:33 +0000

    violet: AEX-ify
    - Drop unused WFD packages
    - Don't enforce RRO on framework-res because it breaks some stuff.
    - Label a bluetooth prop which was earlier labelled in device_lineage_sepolicy
    
    Signed-off-by: Atman <masteratman@gmail.com>

--- device.mk
+++ device.mk
@@ -9,11 +9,7 @@ $(call inherit-product-if-exists, vendor/xiaomi/violet/violet-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+    $(LOCAL_PATH)/overlay
 
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
