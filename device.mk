410ee7ac9d3b84e3a1e165806063fc7f178865c9
Logan <athuldineshan5@gmail.com>
2020-04-11 15:25:03 +0000

    violet:Introduce Miuicamera
    Signed-off-by: Logan <athuldineshan5@gmail.com>

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@ $(call inherit-product, vendor/xiaomi/violet/firmware/firmware.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
+# MiuiCamera
+$(call inherit-product-if-exists, vendor/MiuiCamera/config.mk)
+
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
